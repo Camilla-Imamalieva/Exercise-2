12a2a7989b00b6df2055ca03d84764df1a21185c
Camillar <camillacrew@mail.ru>
2023-02-19 23:16:26 +0400

    Добавили папку Exercise_1

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int secondRank = num / 10 % 10;
+Console.WriteLine($"Вторая цифра {secondRank} ");
