7201dae58e6bce72b1625b99797e13f35a818f3e
Camillar <camillacrew@mail.ru>
2023-02-22 13:16:27 +0400

    Добавили папку Exercise_2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+int Prompt(string message)
+{
+Console.Write("Введите любое число: ");
+string value = Console.ReadLine();
+int result = Convert.ToInt32(value);
+return result;
+}
+
+int GetThirdRank(int num)
+{
+   while (num > 999)
+   {
+      num /= 10;
+   }
+   return num % 10;
+}
+
+bool ValidateNumber (int num) 
+{
+   if (num < 100)
+   {
+      Console.WriteLine("Третий цифры нет");
+      return false;
+   }
+   return true;
+}    
+
+int num = Prompt("Введите число: ");
+if (ValidateNumber(num))
+{
+   Console.WriteLine(GetThirdRank(num));
+}  
