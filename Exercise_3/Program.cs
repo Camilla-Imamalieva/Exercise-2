58dde553b14c64ca91af36bd902fd12227958ea8
Camillar <camillacrew@mail.ru>
2023-02-22 14:30:10 +0400

    Добавили папку Exercise_3

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+int Prompt(string message)
+{
+Console.Write("Введите любое число: ");
+string value = Console.ReadLine();
+int result = Convert.ToInt32(value);
+return result;
+}
+
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekDay(int num)
+{
+    if (num > 0 && num <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели!");
+    return false;
+}
+int weekDay = Prompt ("Введите любое число: ");
+if (ValidateWeekDay(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Это выходной день");
+    }
+    else
+    {
+        Console.WriteLine("Это не выходной день");
+    }
+}
